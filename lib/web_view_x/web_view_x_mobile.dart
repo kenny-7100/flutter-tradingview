746ccee44d874dcb77aed02af4b79a133dd1aa5c
jimao <jimao@jimaodeMac-mini-2.local>
2026-01-28 10:58:51 +0800

    feat: change webview

--- lib/web_view_x/web_view_x_mobile.dart
+++ lib/web_view_x/web_view_x_mobile.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:webview_flutter/webview_flutter.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 
 class WebViewX extends StatefulWidget {
   const WebViewX({super.key, required this.url, this.width, this.height});
@@ -13,22 +13,21 @@ class WebViewX extends StatefulWidget {
 }
 
 class _WebViewXState extends State<WebViewX> {
-  late final WebViewController _controller;
-
-  @override
-  void initState() {
-    super.initState();
-    _controller = WebViewController()
-      ..setJavaScriptMode(JavaScriptMode.unrestricted)
-      ..loadRequest(Uri.parse(widget.url));
-  }
+  InAppWebViewSettings settings = InAppWebViewSettings(
+    javaScriptEnabled: true,
+    mediaPlaybackRequiresUserGesture: false,
+    allowsInlineMediaPlayback: true,
+  );
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       width: widget.width ?? double.infinity,
       height: widget.height ?? double.infinity,
-      child: WebViewWidget(controller: _controller),
+      child: InAppWebView(
+        initialUrlRequest: URLRequest(url: WebUri(widget.url)),
+        initialSettings: settings,
+      ),
     );
   }
 }
